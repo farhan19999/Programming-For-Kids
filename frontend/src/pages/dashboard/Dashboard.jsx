28461e3c6111f1270e1908b336f14089a83a0034
Mahbub <mahbub.buet.cse18@gmai.com>
2023-08-10 18:57:45 +0600

    footer updated

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../../components/navbar/Navbar';
-import Footer from '../../components/footer/Footer'
+import Footer from '../../components/footer/Footer';
+import UserDetails from '../../components/userdetails/UserDetails';
 
 function ShowDashboard() {
   return (
     <div>
       <Navbar/>
+      <UserDetails/>
       <Footer/>
     </div>
   );
