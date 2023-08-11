6b94f30c3c7d9a1db34513af2b6706341d8a35f2
ariffaisal-github <1805118@ugrad.cse.buet.ac.bd>
2023-08-12 02:36:34 +0600

    admin miniproject index added

--- frontend/src/pages/admin_miniproject_index/AdminMPindex.jsx
+++ frontend/src/pages/admin_miniproject_index/AdminMPindex.jsx
@@ -6,7 +6,7 @@ export default function AdminMPindex() {
     <div>
         <Navbar />
         <h2 style={{margin:"25px",fontWeight:"bold"}}>Admin</h2>
-        <table class="table table-striped" style={{margin:"25px",fontSize:"18px"}}>
+        <table class="table table-hover" style={{margin:"25px",fontSize:"18px"}}>
   <thead>
   </thead>
   <tbody>
