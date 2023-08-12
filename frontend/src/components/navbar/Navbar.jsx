3fc98a356cf364c6c34f7f722685bc439dcf827b
ariffaisal-github <1805118@ugrad.cse.buet.ac.bd>
2023-08-12 20:31:14 +0600

    modified

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -3,37 +3,37 @@ import React from 'react';
 
 function Navbar() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Programming For Kids</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Programming For Kids</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="/home">Dashboard</a>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/home">Dashboard</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="/contest">Contest</a>
+                        <li className="nav-item">
+                            <a className="nav-link active" href="/contest">Contest</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="/input">Practice</a>
+                        <li className="nav-item">
+                            <a className="nav-link active" href="/input">Practice</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="/puzzle">Daily-Puzzle</a>
+                        <li className="nav-item">
+                            <a className="nav-link active" href="/puzzle">Daily-Puzzle</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="/game">Code-Game</a>
+                        <li className="nav-item">
+                            <a className="nav-link active" href="/game">Code-Game</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="/StudentProfile">Student-Profile</a>
+                        <li className="nav-item">
+                            <a className="nav-link active" href="/StudentProfile">Student-Profile</a>
                         </li>
 
-                        <li class="nav-item">
-                            <a class="nav-link" href="/signup">Sign-Up</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/signup">Sign-Up</a>
                         </li>
                     </ul>
                 </div>
