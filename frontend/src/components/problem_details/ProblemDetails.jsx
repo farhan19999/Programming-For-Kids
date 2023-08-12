67b0f6e69f139d24595aafae9a2363e9424ceb46
ariffaisal-github <1805118@ugrad.cse.buet.ac.bd>
2023-08-12 21:12:43 +0600

    updated

--- frontend/src/components/problem_details/ProblemDetails.jsx
+++ frontend/src/components/problem_details/ProblemDetails.jsx
@@ -2,7 +2,8 @@
 
 
 import CodeEditor from '../code_editor/CodeEditor';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 const ProblemDetails = () => {
