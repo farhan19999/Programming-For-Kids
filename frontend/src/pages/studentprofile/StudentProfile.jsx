f06959d029a065ac4137d4db7b971c3a260dc406
Mahbub <mahbub.buet.cse18@gmai.com>
2023-08-14 11:56:36 +0600

    student profile added

---
+++ frontend/src/pages/studentprofile/StudentProfile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../../components/navbar/Navbar';
+import UserProfile from '../../components/update_profile/Profile'
+//Author: MAHBUB
+
+function StudentProfile() {
+  return (
+    <div>
+      <Navbar/>
+      <UserProfile/>
+    </div>
+  );
+}
+
+export default StudentProfile;
