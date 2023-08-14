2c0d7546564682cf3b74cd599d3e6c37f706d4c8
Mahbub <mahbub.buet.cse18@gmai.com>
2023-08-14 12:29:52 +0600

    profile page updated

--- frontend/src/components/update_profile/Profile.jsx
+++ frontend/src/components/update_profile/Profile.jsx
@@ -20,6 +20,35 @@ function Profile() {
 		})
 	  },[]);
 
+	  const [userData, setUserData] = useState({
+		userid:user.userid,
+		username: user.username,
+		password: '',
+		rating: user.rating,
+		ranking: user.ranking,
+		phone_no: '',
+		email_address: ''
+	  });
+	
+	  const handleFormSubmit = async (e) => {
+		e.preventDefault();
+		const password=document.getElementById("password").value;
+		const phone_no=document.getElementById("phone").value;
+		const email_address=document.getElementById("email").value;
+		console.log(email_address);
+		try {
+		  // Handle validations
+		  axios
+			.put("http://localhost:3000/api/users/"+user.userid, { username,password,phone_no,email_address, rating, ranking })
+			.then(response => {
+			  console.log(response);
+			  window.location.href = "/";
+			});
+		} catch (error) {
+		  console.error('Error creating user:', error);
+		}
+	  };
+
 	return (
 		<div className="container" style={{marginTop:"25px"}}>
 			<div className="row gutters">
@@ -33,10 +62,6 @@ function Profile() {
 									</div>
 									<h5 className="user-name">{user.username}</h5>
 									<h6 className="user-email">{user.email_address}</h6>
-								</div>
-								<div className="about">
-									<h5>About</h5>
-									<p>I'm {user.username}. Full Stack Designer I enjoy creating user-centric, delightful and human experiences.</p>
 								</div>
 							</div>
 						</div>
@@ -52,7 +77,7 @@ function Profile() {
 								<div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
 									<div className="form-group">
 										<label for="userName">UserName</label>
-										<input type="text" className="form-control" id="userName" placeholder={user.username} />
+										<input type="text" className="form-control" id="userName" placeholder={user.username} readOnly={true} />
 									</div>
 								</div>
 								<div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
@@ -77,19 +102,19 @@ function Profile() {
 								<div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
 									<div className="form-group">
 										<label for="password">Old Password</label>
-										<input type="password" className="form-control" id="password" placeholder="Enter Old Password" />
+										<input type="password" className="form-control" id="password1" placeholder="Enter Old Password" />
 									</div>
 								</div>
 								<div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
 									<div className="form-group">
 										<label for="eMail">New Password</label>
-										<input type="email" className="form-control" id="eMail" placeholder="Enter New Password" />
+										<input type="email" className="form-control" id="password" placeholder="Enter New Password" />
 									</div>
 								</div>
 								<div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
 									<div className="form-group">
 										<label for="phone">Confirm New Password</label>
-										<input type="text" className="form-control" id="phone" placeholder="Confirm New Password" />
+										<input type="text" className="form-control" id="password" placeholder="Confirm New Password" />
 									</div>
 								</div>
 							</div>
@@ -101,7 +126,7 @@ function Profile() {
 								<div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
 									<div className="text-right">
 										<button type="button" id="submit" name="submit" className="btn btn-dark">Cancel</button>
-										<button type="button" id="submit" name="submit" className="btn btn-dark">Update</button>
+										<button type="button" id="submit" name="submit" className="btn btn-dark"  onClick={handleFormSubmit}>Update</button>
 									</div>
 								</div>
 							</div>
