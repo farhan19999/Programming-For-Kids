04fb87aa8e5800b584eecbe6b9bb343ab40e4c62
ariffaisal-github <1805118@ugrad.cse.buet.ac.bd>
2023-08-16 12:25:21 +0600

    option to upload file to dropbox added in CodeEditor.jsx

--- frontend/src/components/code_editor/CodeEditor.jsx
+++ frontend/src/components/code_editor/CodeEditor.jsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import AceEditor from "react-ace";
+import axios from "axios";
 
 // Import ace themes and modes here if needed
 import "ace-builds/src-noconflict/theme-monokai";
@@ -12,16 +13,48 @@ import "ace-builds/src-noconflict/mode-c_cpp";
 const CodeEditor = () => {
   const [selectedLanguage, setSelectedLanguage] = useState("java"); // Default language
   const [submissionStatus, setSubmissionStatus] = useState("Pending"); // Submission status
+  const [codeContent, setCodeContent] = useState(""); // Store the code content
 
   const handleLanguageChange = (event) => {
     setSelectedLanguage(event.target.value);
   };
+    const handleCodeChange = (newCode) => {
+    setCodeContent(newCode);
+  };
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     // Simulate submission logic here
-    const isCorrect = false; 
+    const isCorrect = false;
     // Replace with actual submission logic
     setSubmissionStatus(isCorrect ? "Accepted" : "Wrong Answer");
+    
+
+    try {
+      const accessToken = "sl.BkMBX6FaxcXuZIt1oUu_wufKB635Ph8WRyWdT9c3i49Bzl42NDU0kxmu5zY4fg_3gdh7O8dP61JOXFiWz46ltGLer3ZW8R0LvvnPM5rSEf0GUUZAwFwpYNrzjfVlL2K2N7OqoHrguuzW"; // Replace with your Dropbox access token
+      const content = new Blob([codeContent], { type: "text/plain" });
+
+      // Upload the code content to Dropbox
+      await axios.post(
+        "https://content.dropboxapi.com/2/files/upload",
+        content,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/octet-stream",
+            "Dropbox-API-Arg": JSON.stringify({
+              path: "/a.cpp", // Specify the path and filename
+              mode: "overwrite",
+            }),
+          },
+        }
+      );
+
+      console.log("Code uploaded to Dropbox");
+      console.log("Code content:", codeContent);
+    } catch (error) {
+      console.error("Error uploading to Dropbox:", error);
+      
+    }
   };
 
   return (
@@ -35,13 +68,12 @@ const CodeEditor = () => {
             value={selectedLanguage}
             onChange={handleLanguageChange}
           >
+            <option value="c_cpp">C++</option> {/* Added C++ option */}
             <option value="java">Java</option>
             <option value="python">Python</option>
-            <option value="c_cpp">C++</option> {/* Added C++ option */}
             {/* Add more language options here */}
           </select>
         </div>
-        
       </div>
 
       <div style={editorContainerStyle}>
@@ -54,43 +86,55 @@ const CodeEditor = () => {
             fontSize={14}
             fontColor="#666"
             style={{ backgroundColor: "#555" }} //  background color
+            onChange={handleCodeChange}
           />
         </div>
       </div>
 
       {/* Add a submit button */}
       <div style={submitButtonContainerStyle} class="d-flex ">
-        <button onClick={handleSubmit} type="button" class="btn btn-dark btn-me">Submit</button>
+        <button
+          onClick={handleSubmit}
+          type="button"
+          class="btn btn-dark btn-me"
+        >
+          Submit
+        </button>
       </div>
       {submissionStatus && (
         <div style={statusBoxStyle}>
           <span>Status: </span>
-          <span style={{ color: submissionStatus === "Accepted" ? "green" : submissionStatus === "Pending" ? "blue":"red" }}>
+          <span
+            style={{
+              color:
+                submissionStatus === "Accepted"
+                  ? "green"
+                  : submissionStatus === "Pending"
+                  ? "blue"
+                  : "red",
+            }}
+          >
             {submissionStatus}
           </span>
         </div>
       )}
-
-
     </div>
   );
 };
 
 const statusBoxStyle = {
-    marginTop: "10px",
-    display: "flex",
-    alignItems: "center",
-    color: "#333",
-    marginBottom: "20px",
-  };
+  marginTop: "10px",
+  display: "flex",
+  alignItems: "center",
+  color: "#333",
+  marginBottom: "20px",
+};
 
 const submitButtonContainerStyle = {
   display: "flex",
   justifyContent: "flex-start", // Align button to the right
   marginTop: "10px",
 };
-
-
 
 const editorContainerStyle = {
   display: "flex",
