c6db16fd07f07d2f975fdc7ca64620d29e3f8d8a
Mahbub <mahbub.buet.cse18@gmai.com>
2023-08-27 11:49:03 +0600

    admin miniproject details modified

--- frontend/src/pages/admin/admin_miniproject_details/AdminMPdetails.jsx
+++ frontend/src/pages/admin/admin_miniproject_details/AdminMPdetails.jsx
@@ -1,62 +1,82 @@
 // Arif
 
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import AceEditor from "react-ace";
-import "ace-builds/src-noconflict/mode-c_cpp";
-import "ace-builds/src-noconflict/theme-monokai";
-import Navbar from "../../../components/navbar/Navbar";
-import Timer from "../../../components/time_remaining/Timer";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useParams } from "react-router-dom";
+import Navbar from "../../../components/navbar/Navbar";
+import Footer from "../../../components/footer/Footer";
+import { useNavigate } from "react-router-dom";
 
-const AdminMPDetails = () => {
-  const [isEditing, setIsEditing] = useState(false);
-  const [code, setCode] = useState("");
+export default function AdminMPDetails() {
+  const defaultState = {
+  };
 
   const { projectid } = useParams();
 
-  const handleCodeChange = (newCode) => {
-    setCode(newCode);
+  const [project, setProject] = useState('');
+  const [projectDetails, setProjectDetails] = useState('');
+  const [code, setCode] = useState('');
+
+
+  const handleProjectDetailsChange = (event) => {
+    setProjectDetails(event.target.value);
+  };
+  
+  const handleCodeChange = (event) => {
+    setCode(event.target.value);
   };
 
-  const handleEditButtonClick = () => {
-    setIsEditing(true);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/api/mini-projects/${projectid}`)
+      .then((response) => {
+        setProject(response.data);
+        setCode(response.data.starting_code);
+        setProjectDetails(response.data.project_details);
+        console.log(response.data);
+      });
+  }, [projectid]);
+
+  const navigate = useNavigate();
+  const handleCancelClick = () => {
+    navigate(`/admin/miniprojects`);
   };
 
-  const handleSaveButtonClick = () => {
-    // Implement save logic here
+  const handleDeleteClick = () => {
+    axios
+      .delete(
+        `http://localhost:3000/api/mini-projects/${projectid}`
+      )
+      .then((response) => {
+        console.log("Problem deleted:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error deleting problem:", error);
+      });
+    navigate(`/admin/miniprojects`);
+  }
+
+  const handleSaveClick = () => {
     axios
       .put(`http://localhost:3000/api/mini-projects/${projectid}`, {
         starting_code: code, // Pass the updated code to the backend
-        title: problem.title,
-        project_details: problem.project_details,
-        starting_time: problem.starting_time,
+        title: project.title,
+        project_details: projectDetails,
+        starting_time: project.starting_time,
       })
       .then((response) => {
         console.log("Code saved:", response.data);
-        setIsEditing(false);
       })
       .catch((error) => {
         console.error("Error saving code:", error);
       });
 
     console.log("Saving code:", code);
-    setIsEditing(false);
+    navigate(`/admin/miniprojects`);
   };
 
-  const [problem, setProblem] = useState({});
-  useEffect(() => {
-    axios
-      .get(`http://localhost:3000/api/mini-projects/${projectid}`)
-      .then((response) => {
-        setProblem(response.data);
-        setCode(response.data.starting_code);
-        console.log(response.data);
-      });
-  }, [projectid]);
-
   return (
-    <div style={containerStyle}>
+    <div style={{ position: "relative" }}>
       <Navbar />
       <h2
         style={{
@@ -67,104 +87,93 @@ const AdminMPDetails = () => {
       >
         Admin
       </h2>
-      <h3 style={{ textAlign: "center" }}>Mini Project: {problem.title}</h3>
-      <Timer />
+      <h3 style={{ textAlign: "center" }}>Mini Project: {project.title}</h3>
 
-      <div style={projectDetailsStyle}>
-        {problem.project_details}
-        <br /> <br />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6" style={{ marginTop: "20px" }}>
+            <div className="form-group">
+              <label htmlFor="problemStatement">
+                <p style={{ fontSize: "18px" }}>Project Details:</p>
+              </label>
+              <textarea
+                style={{ backgroundColor: "#eee", height:"" }}
+                className="form-control"
+                id="problemStatement"
+                rows="15"
+                value={projectDetails}
+                onChange={handleProjectDetailsChange}
+              ></textarea>
+            </div>
+
+          </div>
+          <div className="col-md-6" style={{ marginTop: "18px" }}>
+            <div className="form-group">
+              <label htmlFor="solution">
+                <p style={{ fontSize: "19px" }}>Puzzle Code :</p>
+              </label>
+              <textarea
+                style={{ backgroundColor: "#222", color: "white" }}
+                className="form-control"
+                id="solution"
+                rows="15"
+                value={code}
+                onChange={handleCodeChange}
+              ></textarea>
+            </div>
+          </div>
+        </div>
       </div>
-      <p style={{ marginLeft: "50px", fontWeight: "bold" }}>Code:</p>
-      {isEditing ? (
-        <AceEditor
-          mode="c_cpp"
-          theme="monokai"
-          name="code-editor"
-          fontSize={16}
-          value={code}
-          onChange={handleCodeChange}
-          editorProps={{ $blockScrolling: true }}
-          style={codeBoxStyle}
-        />
-      ) : (
-        <pre style={codeBoxStyle}>{code}</pre>
-      )}
 
-      {!isEditing && (
-        <button
-          type="button"
-          className="btn btn-dark"
-          style={editButtonStyle}
-          onClick={handleEditButtonClick}
-        >
-          Edit
-        </button>
-      )}
+      <button
+        type="button"
+        className="btn btn-dark"
+        style={{
+          position: "relative",
+          bottom: "0px",
+          right: "0",
+          width: "120px",
+          marginLeft: "07%",
+          marginTop: "50px",
+        }}
+        onClick={handleDeleteClick}
+      >
+        Delete
+      </button>
 
-      {isEditing && (
-        <button
-          type="button"
-          className="btn btn-dark"
-          style={saveButtonStyle}
-          onClick={handleSaveButtonClick}
-        >
-          Save
-        </button>
-      )}
+      <button
+        type="button"
+        className="btn btn-dark"
+        style={{
+          position: "relative",
+          bottom: "0px",
+          right: "0",
+          width: "120px",
+          marginLeft: "930px",
+          marginTop: "50px",
+        }}
+        onClick={handleCancelClick}
+      >
+        Cancel
+      </button>
+      <button
+        type="button"
+        className="btn btn-dark"
+        style={{
+          position: "relative",
+          bottom: "0px",
+          right: "0",
+          marginRight: "50px",
+          width: "120px",
+          marginLeft: "10px",
+          marginTop: "50px",
+        }}
+        onClick={handleSaveClick}
+      >
+        Save
+      </button>
+
+      <Footer />
     </div>
   );
-};
-
-const containerStyle = {
-  marginBottom: "60px",
-  display: "flex",
-  flexDirection: "column",
-  position: "relative", // Add position relative to the container
-};
-
-const projectDetailsStyle = {
-  textAlign: "left",
-  marginTop: "20px",
-  marginLeft: "50px",
-  marginRight: "20px",
-  marginBottom: "20px",
-  fontSize: "18px",
-};
-
-const codeBoxStyle = {
-  backgroundColor: "#444",
-  fontSize: "125px",
-  color: "white",
-  padding: "20px",
-  borderRadius: "5px",
-  fontSize: "17px",
-  fontFamily: "monospace",
-  lineHeight: "1.5",
-  display: "block",
-  whiteSpace: "pre-wrap",
-  overflowX: "auto",
-  marginRight: "30px",
-  marginLeft: "50px",
-  width: "800px",
-};
-
-const editButtonStyle = {
-  position: "relative",
-  bottom: "0px",
-  left: "50%",
-  marginBottom: "20px",
-  marginLeft: "-32px",
-  width: "120px",
-};
-
-const saveButtonStyle = {
-  position: "relative",
-  bottom: "0px",
-  right: "0",
-  marginRight: "50px",
-  width: "120px",
-  marginLeft: "730px",
-  marginTop: "5px",
-};
-
-export default AdminMPDetails;
+}
