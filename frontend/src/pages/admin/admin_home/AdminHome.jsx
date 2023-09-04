fa9e7a954bda6abae9ea094ddcf8f2436acb78e6
Arif Faisal <1805118@ugrad.cse.buet.ac.bd>
2023-09-04 14:16:59 +0600

    admin homepage modified

--- frontend/src/pages/admin/admin_home/AdminHome.jsx
+++ frontend/src/pages/admin/admin_home/AdminHome.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import Footer from '../../../components/footer/Footer'
-import AdminNavbar from '../../../components/admin_navbar/AdminNavbar'
+import React from 'react';
+import Footer from '../../../components/footer/Footer';
+import AdminNavbar from '../../../components/admin_navbar/AdminNavbar';
 
 export default function AdminHome() {
   return (
     <div>
-        <AdminNavbar />
-        
-        <Footer /> 
+      <AdminNavbar />
+      <div
+        style={{
+          margin: '20px auto', // Auto margins horizontally center the content
+          marginTop: '100px',
+          width: '80%', // You can adjust the width as needed
+          textAlign: 'center', // Center-align the text
+        }}
+        className="admin-content"
+      >
+        <h1>Welcome to the Admin Dashboard</h1>
+        <p>
+          This is the homepage for admin users. You can manage and control
+          various aspects of the application from here.
+        </p>
+        {/* Add more admin-specific content or functionality here */}
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
